e2c2a2e74b513e1d3330d44d574817a2c5daf81b
Bekn The programmer <vp485454@gmail.com>
2026-05-20 14:37:22 -0300

    update 20/05

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import ProfilePic from './assets/profile-pic.png'
 import SunIcon from './assets/sun-icon.svg'
 import MoonIcon from './assets/moon-icon.svg'
+import FavoriteSongs from './components/favoriteSongs'
 
 function App() {
   const [theme, setTheme] = useState('light')
@@ -36,11 +37,16 @@ function App() {
 
         <h2 className="text-muted">Student (High School)</h2>
         <div id="about-me" className="d-flex flex-column gap-3">
-          <h3><span className="text-primary fw-semibold">Languages</span>: English 🇬🇧 (B1), Portuguese 🇧🇷 (C2 - Native)</h3>
-          <h3><span className="text-primary fw-semibold">Interests</span>: Programming, Gaming, Music, Reading, Languages Learning</h3>
-          <h3><span className="text-primary fw-semibold">Hard Skills (Programming)</span>: HTML, CSS, JavaScript, React</h3>
-          <h3><span className="text-primary fw-semibold">Soft Skills</span>: Problem Solving, Communication, Time Management</h3>
-          <h3><span className="text-primary fw-semibold">Goals</span>: To become a proficient web developer and contribute to open-source projects and work abroad</h3>
+          <h3><span className="text-primary fw-semibold">Languages:</span> English 🇬🇧 (B1), Portuguese 🇧🇷 (C2 - Native)</h3>
+          <h3><span className="text-primary fw-semibold">Interests:</span> Programming, Gaming, Music, Reading, Languages Learning</h3>
+          <h3><span className="text-primary fw-semibold">Hard Skills (Programming):</span> HTML, CSS, JavaScript, React</h3>
+          <h3><span className="text-primary fw-semibold">Soft Skills:</span> Problem Solving, Communication, Time Management</h3>
+          <h3><span className="text-primary fw-semibold">Hobbies:</span> Playing video games, Listening to music, Reading books, Learning about programming languages, 
+          learning spoken languages</h3>
+          <h3><span className="text-primary fw-semibold">MBTI:</span> ENTJ</h3>
+          <h3><span className="text-primary fw-semibold">Favorite Music Genres:</span> Rock, Pop, Rock indie, EDM, Soft Rock</h3>
+          <h3><span className="text-primary fw-semibold">Goals:</span> To become a proficient web developer and contribute to open-source projects and work abroad</h3>
+        <FavoriteSongs />
         </div>
       </div>
     </div>
---
+++ src/components/favoriteSongs.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+
+const favoriteSongs = () => {
+  return (
+    <div id='favorite songs'>
+        <h1 className="fw-bold">Favorite Songs (Don't judge me)</h1>
+
+    
+        <h3> <span className="text-primary fw-semibold">Feel Good Inc</span>  - Gorillaz</h3>
+        <h3> <span className="text-primary fw-semibold">19-2000</span> - Gorillaz</h3>
+        <h3> <span className="text-primary fw-semibold">Feel Fine</span> - Sex bob-omb</h3>
+        <h3> <span className="text-primary fw-semibold">Sussudio</span> - Phil Collins</h3>
+        <h3> <span className="text-primary fw-semibold">Take on me</span> - a-ha</h3>
+        <h3> <span className="text-primary fw-semibold">Heaven</span> - Bryan Adams</h3>
+        <h3> <span className="text-primary fw-semibold">Cicada</span> - Good Kid</h3>
+
+
+
+
+
+    
+
+
+    </div>
+  )
+}
+
+export default favoriteSongs
